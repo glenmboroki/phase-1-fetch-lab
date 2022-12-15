7827381e50b8b98af80dc91359b962814a6d19dd
unknown <lenny>
2022-12-15 11:44:10 +0300

    completed

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then (function(response){
+  return response.json(); 
+  })
+  .then(function(json){
+  renderBooks(json);
+  })
 }
+  // To pass the tests, don't forget to return your fetch!
+  
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
